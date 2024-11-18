a59c6c427449cf0b0f056894c62b7285f3f38306
Geeta Karadkhele <geeta.karadkhele@emory.edu>
2024-11-18 13:26:38 -0500

    documenting a data file

---
+++ R/hla_data.R
@@ -0,0 +1,15 @@
+#' HLA raw data
+#'
+#' Subject identifier and HLA typing data for Recipients and Donors
+#' @format Data frame with 2 columns and 10
+#' \describe{
+#' \item{data}{A dataframe},
+#' \item{var_1}{Character HLA typing value 1},
+#' \item{var_2}{Character HLA typing value 2}
+#' }
+#'
+#' @examples
+#' # example code
+#'    hla_data
+#'
+  "hla_data"
